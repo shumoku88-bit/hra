98ecfa3149e7836904f42346443c6ec528206718
shumoku88-bit <shumoku88@gmail.com>
2026-08-15 22:30:57 +0900

    feat(report): add planned payments renderer

---
+++ src/aledger-planned_payments_render.ads
@@ -0,0 +1,8 @@
+with ALedger.Planned_Payments;
+
+package ALedger.Planned_Payments_Render is
+
+   function Render
+     (Value : ALedger.Planned_Payments.Observation) return String;
+
+end ALedger.Planned_Payments_Render;
