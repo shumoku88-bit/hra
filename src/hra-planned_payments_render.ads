ebc7312f56e4fa2153ce24d5a0f52cdbef097c50
shumoku88-bit <shumoku88@gmail.com>
2026-08-21 10:54:33 +0900

    feat(report): render Planned Payments availability

--- src/hra-planned_payments_render.ads
+++ src/hra-planned_payments_render.ads
@@ -1,8 +1,13 @@
+with HRA.Household_Report_Observation;
 with HRA.Planned_Payments;
 
 package HRA.Planned_Payments_Render is
 
    function Render
      (Value : HRA.Planned_Payments.Observation) return String;
+
+   function Render
+     (Value : HRA.Household_Report_Observation.Planned_Payments_Report_Observation)
+      return String;
 
 end HRA.Planned_Payments_Render;
